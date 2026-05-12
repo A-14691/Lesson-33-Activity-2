493ff6d2aad263fc48cfea140c28955d1fa51f53
A-14691 <adityamskadam@gmail.com>
2026-05-12 17:15:19 +0400

    initial commit

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ pygame.display.set_caption("Adding image and Background image")
 
 background_image=pygame.transform.scale(pygame.image.load('background.png').convert(),(SCREEN_WIDTH, SCREEN_HEIGHT))
 
-penguin_image=pygame.transform.scale(pygame.image.load('penguin.png').convert_alph(),(200, 200))
+penguin_image=pygame.transform.scale(pygame.image.load('penguin.png').convert_alpha(),(200, 200))
 penguin_rect=penguin_image.get_rect(center=(SCREEN_WIDTH//2, SCREEN_HEIGHT //2-30))
 
 # Initialize font, render text, and set text position
